65097a745ac6b6308bf98c182552f207d32af170
todd-bowers <toddbows@gmail.com>
2022-06-28 19:58:37 -0400

    donezo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a number: ");
+        int n = scanner.nextInt();
+        int result = 0;
+
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        System.out.println(result);
     }
 }
